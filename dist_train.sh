0998533ac9ee2a55e2dd2cedbc95973df53deda1
jhkim <kimjeonghyun.jkim@gmail.com>
2023-06-14 22:26:02 +0000

    [CREATE] script files for distributed learning

---
+++ dist_train.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+
+
+GPUS=$1
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.run --nnodes=$NNODES --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR \
+ --nproc_per_node=$GPUS --master_port=$PORT $(dirname "$0")/train_custom.py  --cuda -dist -d ava_v2.2 --world_size 2 ${@:3}
+. 
