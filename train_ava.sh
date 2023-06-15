4a5c30183f099f0f4b47c9bb6502ccce5058f8a4
jhkim <kimjeonghyun.jkim@gmail.com>
2023-06-15 12:56:15 +0000

    [CUTOM] arguments

--- train_ava.sh
+++ train_ava.sh
@@ -1,16 +1,18 @@
 # Train YOWOv2 on AVA dataset
-python train.py \
+python train_custom.py \
         --cuda \
         -d ava_v2.2 \
-        -v yowo_v2_large \
-        --root /mnt/share/sda1/dataset/STAD/ \
+        -v yowo_v2_tiny \
+        --root /nvme0/dev/ \
         --num_workers 4 \
         --eval_epoch 1 \
         --eval \
+        --eval_first \
         --max_epoch 9 \
         --lr_epoch 3 4 5 6 \
         -lr 0.0001 \
         -ldr 0.5 \
-        -bs 8 \
+        -bs 16 \
         -accu 16 \
-        -K 16
+        -K 16 \
+        --save_dir
