79e287ccbc8bd668dee8870ee9eb6abff4badc32
jhkim <kimjeonghyun.jkim@gmail.com>
2023-06-15 12:56:39 +0000

    [TYPO] fix

--- dist_train.sh
+++ dist_train.sh
@@ -12,4 +12,3 @@ MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.run --nnodes=$NNODES --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR \
  --nproc_per_node=$GPUS --master_port=$PORT $(dirname "$0")/train_custom.py  --cuda -dist -d ava_v2.2 --world_size 2 ${@:3}
-. 
